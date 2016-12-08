c585367920fe76b08a0233e9016ced7cd72ac5fd
cuilong <cuilong@nationsky.com>
2016-12-08 18:06:59 +0800

    Init

---
+++ src/main/java/com/cl/CluserApplication.java
@@ -0,0 +1,12 @@
+package com.cl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CluserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CluserApplication.class, args);
+	}
+}
